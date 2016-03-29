e652a14e15f039b05ccaa1e0d296e4ed05c83031
lecongthang <lecongthang.1102@gmail.com>
2016-03-29 12:00:07 -0400

    refactor

--- php/getrequirements.php
+++ php/getrequirements.php
@@ -1,12 +1,28 @@
 <?php
 require("data.php");
-$data = array_merge($ULR, $PR, $UE);
-//echo json_encode($data);
-/*		//$_GET
-$adm_year = $_GET("admission");
-$req = getRequirements($adm_year);
-*/
-$adm_year = 3;
+
+$adm_year = $_REQUEST["adm_year"];
+$adm_year = "1314";				//fake
+
+$major = $_REQUEST["major"];
+$major = "CS";			//fake
+
+$focus_area = $_REQUEST["focus_area"];
+$focus_area = "SE";		//fake
+//$focus_mod = getFocusMod($focus_area);
+$focus_mod = array("CS2103"=>4,"CS3213"=>4,"CS3219"=>4,"CS4211"=>4,"CS4218"=>4);
+
+//$sci_mod = getScienceMod("$major");
+$sci_mod = array("CM1121"=>4,"CM1131"=>4,"CS1417"=>4,"LSM1301"=>4,"LSM1302"=>4,"PC1141"=>4,"PC1142"=>4,"PC1143"=>4,"PC1144"=>4,"PC1221"=>4,"PC1222"=>4,"PC1432"=>4,"MA2213"=>4,"MA2214"=>4,"CM1101"=>4,"CM1111"=>4,"CM1161"=>4,"CM1191"=>4,"CM1401"=>4,"CM1402"=>4,"CM1501"=>4,"CM1502"=>4,"LSM1303"=>4,"PC1421"=>4,"PC1431"=>4,"PC1433"=>4,"MA1104"=>4,"MA2101"=>4,"MA2108"=>4,"MA2501"=>4,"ST2132"=>4,"ST2137"=>4);
+
+$modules = json_decode($_REQUEST["modules"],true);		//type = nil for exempted mods
+$modules = array(array("CS1010","PR", "4"),array("CS1231","PR", "4"),array("GEK1517","ULR","4"),array("MA1101R","PR","4"),array("MA1521","PR","4"),array("CS1020","PR","4"),array("ES1102","UE","0"),array("MA2101","PR","4"),array("MA2213","PR","4"),array("SSA1202","ULR","4"),array("ST2334","PR","4"),array("CS2010","PR","4"),array("CS2100","PR","4"),array("GEM2900","ULR","4"),array("LAC1201","ULR","4"),array("MA2214","PR","4"),array("CS2101","PR","4"),array("CS2102","UE","4"),array("CS2103","PR","4"),array("CS2105","PR","4"),array("IS1103","PR","4"),array("CS2106","PR","4"),array("CS3201","PR","4"),array("CS3202","PR","4"),array("CS3230","PR","4"),array("CS4211","PR","4"),array("CS3240","UE","4"),array("CS3241","UE","4"),array("CS3223","UE","4"),array("CS3243","UE","4"),array("CS3226","UE","4"),array("GEK1544","ULR","4"),array("PC1221","nil","4"),array("MA1301","nil","4"));
+
+$file = "/var/www/html/main/req/CS/".$adm_year.".json";
+
+
+
+/*$adm_year = 3;
 $and1 = array("ULR"=>20);
 $and2 = array("PR"=>array("CS1010"=>4, "CS1020"=>4,"CS2010"=>4,"CS1231"=>4,"CS2100"=>4,"CS2103"=>4,"CS2105"=>4,"CS2106"=>4,"CS3230"=>4,"Focus"=>12,"Focus4"=>4,"Lev4"=>12,"IS1103"=>4,"CS2101"=>4,"MA1301"=>4,"ST2334"=>4,"MA1521"=>4,"MA1101R"=>4,"Scie"=>12));
 $and3 = array("UE"=>20);
@@ -15,23 +31,14 @@ $group1 = array(array("CS3201,CS3202","8"),array("CS3216,CS3217","8"),array("CS3
 $group3 = array(array("CP4101","12"),array("Lev4","12"),array("ATAP","12"));			//not handle yet case Lev4 in or
 $group2 = array(array("PC1221","4"),array("PC1222","4"));
 $or = array($group1, $group2, $group3);
-$req = array($and, $or);
-//$_GET
-$focus_area = "SE";
-$focus_mod = array("CS2103"=>4,"CS3213"=>4,"CS3219"=>4,"CS4211"=>4,"CS4218"=>4);
-//$_GET
-$modules = array(array("CS1010","PR", "4"),array("CS1231","PR", "4"),array("GEK1517","ULR","4"),array("MA1101R","PR","4"),array("MA1521","PR","4"),array("CS1020","PR","4"),array("ES1102","UE","0"),array("MA2101","PR","4"),array("MA2213","PR","4"),array("SSA1202","ULR","4"),array("ST2334","PR","4"),array("CS2010","PR","4"),array("CS2100","PR","4"),array("GEM2900","ULR","4"),array("LAC1201","ULR","4"),array("MA2214","PR","4"),array("CS2101","PR","4"),array("CS2102","UE","4"),array("CS2103","PR","4"),array("CS2105","PR","4"),array("IS1103","PR","4"),array("CS2106","PR","4"),array("CS3201","PR","4"),array("CS3202","PR","4"),array("CS3230","PR","4"),array("CS4211","PR","4"),array("CS3240","UE","4"),array("CS3241","UE","4"),array("CS3223","UE","4"),array("CS3243","UE","4"),array("CS3226","UE","4"),array("GEK1544","ULR","4"),array("PC1221","nil","4"),array("MA1301","nil","4"));
-//science mods (check last by remaining mods?)
-$sci_mod = array("CM1121"=>4,"CM1131"=>4,"CS1417"=>4,"LSM1301"=>4,"LSM1302"=>4,"PC1141"=>4,"PC1142"=>4,"PC1143"=>4,"PC1144"=>4,"PC1221"=>4,"PC1222"=>4,"PC1432"=>4,"MA2213"=>4,"MA2214"=>4,"CM1101"=>4,"CM1111"=>4,"CM1161"=>4,"CM1191"=>4,"CM1401"=>4,"CM1402"=>4,"CM1501"=>4,"CM1502"=>4,"LSM1303"=>4,"PC1421"=>4,"PC1431"=>4,"PC1433"=>4,"MA1104"=>4,"MA2101"=>4,"MA2108"=>4,"MA2501"=>4,"ST2132"=>4,"ST2137"=>4);
-verifyReq($modules,$req, $focus_mod, $sci_mod);
-/*
-$sci = getScienceMod();
-$focus_area = $_GET["focus_area"];
-$focus_mod = getFocusMod($focus_area);
-$modules = $_GET["modules"];
-$modList = json_decode($modules);
-*/
+$req = array("and"=>$and,"or"=>$or);*/
 
+$data = file_get_contents($file);
+
+$req = json_decode($data,true);
+
+
+verifyReq($modules,$req, $focus_mod, $sci_mod);
 
 
 function verifyReq($modules, $req, $focus_mod, $sci_mod){
@@ -44,12 +51,9 @@ function verifyReq($modules, $req, $focus_mod, $sci_mod){
 		$modulesMC[$modName] = intval($modules[$i][2]);
 	}
 
-	$and;
-	$or;
-	for ($i = 0; $i < 2; $i++){
-		$and = $req[0];
-		$or = $req[1]; 
-	}
+	$and = $req["and"];
+	$or = $req["or"]; 
+	
 
 	$count = array();
 	for ($i = 0; $i < count($modules); $i++){
