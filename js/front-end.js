1890d8ae5eea0d2b244b5f0d3cf1078afcc686fb
lecongthang <lecongthang.1102@gmail.com>
2016-04-03 10:29:26 +0800

    Modify front end

--- js/front-end.js
+++ js/front-end.js
@@ -1,14 +1,33 @@
+/*------------------------Drag and Drop Feature---------------------*/
 $(function() {
 	$("#sortable-1, #sortable-2, #sortable-3, #sortable-4").sortable({
 		connectWith: "#sortable-1, #sortable-2, #sortable-3, #sortable-4"
 	});
 });
 
-$(document).ready(function() {
-	$("#deg-req-div").hide();
+function showDegReq() {
+	$("#page-title span").html("Degree Requirement");
+
+	$("#deg-req-div").show();
 	$("#taken-mod-div").hide();
 	$("#plan-mod-div").hide();
 	$("#exempt-mod-div").hide();
+}
+
+$("#get-started-btn").on("click", function() {
+	$("#start-text-div").hide();
+	$("#grad-cer-div").show();
+});
+
+$("#starter-confirm-btn").on("click", function() {
+	$("#start-page").hide();
+	$("#main-section").show();
+	showDegReq();
+});
+
+$(document).ready(function() {
+	$("#main-section").hide();
+	$("#grad-cer-div").hide();
 
 	 // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
 	 $(".modal-trigger").leanModal();
@@ -27,21 +46,27 @@ $(document).ready(function() {
 	 	});
 	 });
 
-	$("#homepage-btn").on("click", function() {
-		$("#home-page-div").hide();
-		$("#deg-req-div").show();
+	 $("#homepage-btn").on("click", function() {
+	 	$("#home-page-div").hide();
+	 	$("#deg-req-div").show();
+	 });
+
+	// Highlight selected li item
+	var selector = ".collapsible-body ul li";
+
+	$(selector).on("click", function(){
+		$(selector).removeClass("active");
+		$(this).addClass("active");
 	});
 });
 
 $("#deg-req-nav").on("click", function (){
-	$("#deg-req-div").show();
-	$("#taken-mod-div").hide();
-	$("#plan-mod-div").hide();
-	$("#exempt-mod-div").hide();
-
+	showDegReq();
 });
 
 $("#taken-mod-nav").on("click", function (){
+	$("#page-title span").html("Modules Taken");
+
 	$("#deg-req-div").hide();
 	$("#taken-mod-div").show();
 	$("#plan-mod-div").hide();
@@ -49,6 +74,8 @@ $("#taken-mod-nav").on("click", function (){
 });
 
 $("#plan-mod-nav").on("click", function (){
+	$("#page-title span").html("Modules Planner");
+
 	$("#deg-req-div").hide();
 	$("#taken-mod-div").hide();
 	$("#plan-mod-div").show();
@@ -56,6 +83,8 @@ $("#plan-mod-nav").on("click", function (){
 });
 
 $("#exempt-mod-nav").on("click", function (){
+	$("#page-title span").html("Modules Exemption");
+
 	$("#deg-req-div").hide();
 	$("#taken-mod-div").hide();
 	$("#plan-mod-div").hide();
