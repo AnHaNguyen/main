aa7d0b6deee39ad5d49d4ca284c51cbfe677afbe
lecongthang <lecongthang.1102@gmail.com>
2016-04-12 10:13:04 -0400

    add finding deleted mods

--- php/getmodules.php
+++ php/getmodules.php
@@ -1,5 +1,31 @@
 <?php
-require('data.php');
-$data = array_merge($ULR, $UE, $PR);
-echo json_encode($data);
+
+/* serve to find module that doesn't exist in current list*/
+//api.nusmods.com/2013-2014/1/bulletinModulesRaw.json
+if (isset($_REQUEST['code'])){
+	$code = $_REQUEST['code'];
+}else{
+	$code = "GEM2900";		//test sem1
+	//$code = "CS3226";		//test sem2
+}
+$list = ["http://api.nusmods.com/2015-2016/1/modules/".$code.".json",
+		"http://api.nusmods.com/2015-2016/2/modules/".$code.".json"];
+
+$moduleInfo = findModule($list);
+echo json_encode($moduleInfo);
+
+function findModule($list){
+	for ($i = 0; $i < count($list); $i++){
+		$file = $list[$i];
+		$content = file_get_contents($file);
+		if ($content){
+			$data = json_decode($content,true);
+			$code = $data["ModuleCode"];
+			$title = $data["ModuleTitle"];
+			$MC = $data["ModuleCredit"];
+			return array("code"=>$code, "title"=>$title, "MC"=>$MC);
+		}
+	}
+	return false;
+}
 ?>
