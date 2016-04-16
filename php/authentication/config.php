85a9ef2746741c2fffedcd7287631d91faea1b1e
lecongthang <lecongthang.1102@gmail.com>
2016-04-16 19:58:03 +0800

    responsive modals

--- php/authentication/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_HOSTNAME', 'localhost');	//need hidden
-define('DB_USERID', 'root');
-define('DB_PASSWORD', 'socteamseven');
-define('DB_NAME', 'DATA');
-define('table', 'USER');
-?>
