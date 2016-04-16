9d31b3c0296259455c9b79e00e66c1a928d66276
lecongthang <lecongthang.1102@gmail.com>
2016-04-16 14:11:28 +0800

    animation for fixed bar

--- js/front-end.js
+++ js/front-end.js
@@ -229,6 +229,30 @@ $(document).ready(function() {
 			$(".drag-drop-mc").show();
 		}
 	});
+
+
+	/*--------------------Trong Hiep code - jQueryScript-----------------------*/
+	$(document).ready(function($) {
+		var $banner = $('.banner'), $window = $(window);
+		var $topDefault = parseFloat( $banner.css('top'), 10 );
+			$window.on('scroll', function() {
+				var $top = $(this).scrollTop();
+				$banner.stop().animate( { top: $top + $topDefault }, 500, 'easeOutCirc' );
+			});
+
+		var $wiBanner = $banner.outerWidth() * 2;
+		zindex( $('#wrapper').outerWidth() );
+		$window.on('resize', function() {
+			zindex( $('#wrapper').outerWidth() );
+		});
+		function zindex(maxWidth){
+			if( $window.width() <= maxWidth + $wiBanner ) {
+				$banner.addClass('zindex');
+			} else {
+				$banner.removeClass('zindex');
+			}
+		}
+	});
 });
 
 
